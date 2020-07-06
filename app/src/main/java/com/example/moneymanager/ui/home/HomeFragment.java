4f65e103964b436c58261ffcfa638b529b4806ce
Alexey <leha-antipin2001@mail.ru>
2020-07-06 18:06:58 +0300

    AccountsFragment

--- app/src/main/java/com/example/moneymanager/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/moneymanager/ui/home/HomeFragment.java
@@ -9,16 +9,45 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
 import com.example.moneymanager.R;
+import com.example.moneymanager.ui.AccountsFragment;
+import com.google.android.material.tabs.TabLayout;
 
 public class HomeFragment extends Fragment {
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_home, container, false);
+
+        TabLayout tabLayout = root.findViewById(R.id.tabLayout);
+
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+
+                if (tab.getPosition() == 1) {
+                    Fragment fragment = new AccountsFragment();
+                    FragmentTransaction ft = getFragmentManager().beginTransaction();
+                    ft.add(R.id.nav_host_fragment, fragment);
+                    ft.commit();
+                }
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
         return root;
     }
 }
